fd12a362bbdee79f0f02eecbb5596f52cbbd771b
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-06-05 13:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,34 +12,21 @@ const lookup = {
 
 function rot13(encodedStr){
    // empty string to store the decoded result
-  let decodedString = "";
-
+  let decodedString = [];
+	var arWords=encodedStr.split(" ");
   // Iterate over each character in the encoded string
-  for (let i = 0; i < encodedString.length; i++) {
+  for (let i = 0; i < arWords.length; i++) {
     // Get the ASCII code of the current character
-    const charCode = encodedString.charCodeAt(i);
-
-    // Check if the character is uppercase letter
-    if (charCode >= 65 && charCode <= 90) {
-      // Apply the ROT13 transformation by shifting the character code by 13 places
-      let decodedCharCode = charCode - 13;
-
-      // If the shifted character code goes below the range of uppercase letters (65-90),
-      // wrap it around to the end of the range
-      if (decodedCharCode < 65) {
-        decodedCharCode += 26;
-      }
-
-      // Convert the decoded character code back to a character and append it to the decoded string
-      decodedString += String.fromCharCode(decodedCharCode);
-    } else {
-      // If the character is not an uppercase letter, simply append it to the decoded string
-      decodedString += encodedString.charAt(i);
-    }
+    var newStr="";
+	  var strWord=arWords[i];
+	  for(var j=0;j<strWord.length;j++){
+		  newStr+=lookup[strWord.charAt(j)];
+	  }
+	  decodedString.push(newStr);
   }
 
   // Return the decoded string
-  return decodedString;
+  return decodedString.join(" ");
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
